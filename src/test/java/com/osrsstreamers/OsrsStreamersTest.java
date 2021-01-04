2cd80be46b8e69586c40d8fe8594edc43b78dac5
Roy Porter <royporter7@gmail.com>
2021-01-05 02:02:14 +0900

    Adding distinct tests to streamers config file and fixing config file to be distinct

--- src/test/java/com/osrsstreamers/OsrsStreamersTest.java
+++ src/test/java/com/osrsstreamers/OsrsStreamersTest.java
@@ -1,7 +1,21 @@
 package com.osrsstreamers;
 
+import com.google.gson.Gson;
+import com.osrsstreamers.handler.Streamer;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class OsrsStreamersTest
 {
@@ -9,5 +23,23 @@ public class OsrsStreamersTest
 	{
 		ExternalPluginManager.loadBuiltin(OsrsStreamersPlugin.class);
 		RuneLite.main(args);
+	}
+
+	@Test
+	public void testStreamerNamesUnique() throws FileNotFoundException {
+		BufferedReader br = new BufferedReader(new FileReader("resources/streamers.json"));
+		Streamer[] streamers = new Gson().fromJson(br, Streamer[].class);
+		List<String> twitchNames = Arrays.stream(streamers).map(streamer -> streamer.twitchName).collect((Collectors.toList()));
+		List<String> nonDistinctStreamers = twitchNames.stream().filter(i -> Collections.frequency(twitchNames, i) > 1).collect(Collectors.toList());
+		assertEquals(Collections.EMPTY_LIST, nonDistinctStreamers);
+	}
+
+	@Test
+	public void testCharacterNamesUnique() throws FileNotFoundException {
+		BufferedReader br = new BufferedReader(new FileReader("resources/streamers.json"));
+		Streamer[] streamers = new Gson().fromJson(br, Streamer[].class);
+		List<String> flatCharacterList = Arrays.stream(streamers).flatMap(streamer -> streamer.characterNames.stream()).collect(Collectors.toList());
+		List<String> nonDistinctCharacterNames = flatCharacterList.stream().filter(i -> Collections.frequency(flatCharacterList, i) > 1).collect(Collectors.toList());
+		assertEquals(Collections.EMPTY_LIST, nonDistinctCharacterNames);
 	}
 }
